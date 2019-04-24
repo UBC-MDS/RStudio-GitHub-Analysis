128c1446b7eff31c6945390426224f3f05f70d54
Rayce Rossum <raycer>
2019-04-24 13:58:17 -0700

    Code cleanup

--- src/big_cloud_scratch.py
+++ src/big_cloud_scratch.py
@@ -3,9 +3,10 @@ import networkx as nx
 import matplotlib.pyplot as plt
 import graph2vec as g2v
 import time
+import numpy.distutils.system_info as sysinfo
+
 from joblib import Parallel, delayed
 from gensim.models.doc2vec import Doc2Vec, TaggedDocument
-import numpy.distutils.system_info as sysinfo
 from tqdm import tqdm
 
 def query_ght(queryString):
@@ -28,7 +29,6 @@ def commit_query(projectId):
             limit 10000
         """
 
-
 def git_graph(commits):
     source_target_commits = commits[["cp_parent_id", "c_id"]].dropna().astype("int64")
     source_target_commits.columns = ["source", "target"]
@@ -38,21 +38,23 @@ def git_graph(commits):
 def plot_commits(graph):
     nx.draw_kamada_kawai(graph, alpha=0.5, node_color='blue', node_size = 2)
 
-if __name__ == '__main__':
-    n_workers = 4
-    n_iterations = 1
+n_workers    = 4
+n_iterations = 1
+n_dimensions = 128
 
+if __name__ == '__main__':
     query_p1 = commit_query(22003900)
     query_p2 = commit_query(33470153)
+
     data_p1 = query_ght(query_p1)
-    #print(data_p1)
     data_p2 = query_ght(query_p2)
+
     graphs = [git_graph(data_p1), git_graph(data_p2)]
 
     document_collections = Parallel(n_jobs = n_workers)(delayed(g2v.feature_extractor)(graphs[g], n_iterations, str(g)) for g in tqdm(range(len(graphs))))
 
     model = Doc2Vec(document_collections,
-                    size = 128,
+                    size = n_dimensions,
                     window = 0,
                     min_count = 5,
                     dm = 0,
@@ -61,14 +63,12 @@ if __name__ == '__main__':
                     iter = n_iterations,
                     alpha = 0.025)
 
-    print(model)
-    g2v.save_embedding("./results/embeddings.csv", model, len(graphs), 128)
+    g2v.save_embedding("./results/embeddings.csv", model, len(graphs), n_dimensions)
 
     for graph in graphs:
         plot_commits(graph)
         plt.show()
 
-    #print(document_collections)
     # start = time.time()
     # commits = query_ght(commitQuery)
     # getData = time.time()
--- src/graph2vec.py
+++ src/graph2vec.py
@@ -1,13 +1,7 @@
 import hashlib
 import logging
-import json
-import glob
 import pandas as pd
 import networkx as nx
-from tqdm import tqdm
-from joblib import Parallel, delayed
-from parser import parameter_parser
-from gensim.models.doc2vec import Doc2Vec, TaggedDocument
 import numpy.distutils.system_info as sysinfo
 
 logging.basicConfig(format="%(asctime)s : %(levelname)s : %(message)s", level=logging.INFO)
@@ -53,36 +47,13 @@ class WeisfeilerLehmanMachine:
         for iteration in range(self.iterations):
             self.features = self.do_a_recursion()
 
-
-def dataset_reader(graph):
-    """
-    Function to read the graph and features from a json file.
-    :param path: The path to the graph json.
-    :return graph: The graph object.
-    :return features: Features hash table.
-    :return name: Name of the graph.
-    """
-    name = path.strip(".json").split("/")[-1]
-    data = json.load(open(path))
-    graph = nx.from_edgelist(data["edges"])
-
-    if "features" in data.keys():
-        features = data["features"]
-    else:
-        features = nx.degree(graph)
-
-    features = {int(k):v for k,v, in features.items()}
-    return graph, features, name
-
 def feature_extractor(graph, rounds, name):
     """
     Function to extract WL features from a graph.
-    :param path: The path to the graph json.
+    :param graph: The nx graph.
     :param rounds: Number of WL iterations.
     :return doc: Document collection object.
     """
-    #name = "1"
-    print(name)
     features = nx.degree(graph)
     features = {int(k):v for k,v, in features}
 
@@ -90,13 +61,12 @@ def feature_extractor(graph, rounds, name):
     doc = TaggedDocument(words = machine.extracted_features , tags = ["g_" + name])
     return doc
 
-
 def save_embedding(output_path, model, n_graphs, dimensions):
     """
     Function to save the embedding.
     :param output_path: Path to the embedding csv.
     :param model: The embedding model object.
-    :param files: The list of files.
+    :param n_graphs: The number of graphs used to train the model.
     :param dimensions: The embedding dimension parameter.
     """
     out = []
@@ -106,29 +76,3 @@ def save_embedding(output_path, model, n_graphs, dimensions):
     out = pd.DataFrame(out,columns = ["type"] +["x_" +str(dimension) for dimension in range(dimensions)])
     out = out.sort_values(["type"])
     out.to_csv(output_path, index = None)
-
-
-def main(args):
-    """
-    Main function to read the graph list, extract features, learn the embedding and save it.
-    :param args: Object with the arguments.
-    """
-    graphs = glob.glob(args.input_path + "*.json")
-    print("\nFeature extraction started.\n")
-    document_collections = Parallel(n_jobs = args.workers)(delayed(feature_extractor)(g, args.wl_iterations) for g in tqdm(graphs))
-    print("\nOptimization started.\n")
-    model = Doc2Vec(document_collections,
-                    size = args.dimensions,
-                    window = 0,
-                    min_count = args.min_count,
-                    dm = 0,
-                    sample = args.down_sampling,
-                    workers = args.workers,
-                    iter = args.epochs,
-                    alpha = args.learning_rate)
-
-    save_embedding(args.output_path, model, graphs, args.dimensions)
-
-if __name__ == "__main__":
-    args = parameter_parser()
-    main(args)
